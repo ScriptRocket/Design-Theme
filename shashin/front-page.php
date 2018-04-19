feacd5d4427c988d4270a7bcdc9e512a38ea4c01
Russ Wilkie <russwilkie@gmail.com>
2018-04-19 11:29:25 -0500

    Moved site title and tagline

--- shashin/front-page.php
+++ shashin/front-page.php
@@ -4,5 +4,11 @@ get_header(); ?>
 	
 	<!-- site-content -->
 	<div class="site-content clearfix">
-       
+
+	<h1><?php bloginfo('name'); ?></h1>
+
+	<h5><?php bloginfo('description'); ?> <?php if (is_page('portfolio')) { ?>
+				- Thank you for viewing our work
+			<?php }?></h5>
+
 	</div><!-- /site-content -->
--- shashin/header.php
+++ shashin/header.php
@@ -19,12 +19,6 @@
 				<?php get_search_form(); ?>
 			</div><!-- /hd-search -->
 			
-			<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-			<h5><?php bloginfo('description'); ?> <?php if (is_page('portfolio')) { ?>
-				- Thank you for viewing our work
-			<?php }?></h5>
-			
-			
 			
 			<nav class="site-nav">
 				
